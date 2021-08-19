ef992eda890965c07763b5af250ae6b7959f39dc
Daniil <45428240+shaazmik@users.noreply.github.com>
2021-08-19 22:44:20 +0300

    Add files via upload

--- sqreq.h
+++ sqreq.h
@@ -3,26 +3,30 @@
 #include <assert.h>
 #include <string.h>
 
+//#define DEBUG_MODE
+
+//!@mainpage
 //!Program for solving quadratic equations
-//!An equation of the form ax^2+bx+c=0. 3 coefficients a, b, and c are entered.
+//!An equation of the form ax^2+bx+c=0. 3 coefficients @c a, @c b, and @c c are entered.
 //!If you want, you can turn on the debug. In the header, you need to delete the "/ / " near #define DEBUG_MOD.
 //!The file for debug must be entered with the name Tests.txt. Format:
 //!First line is the number of tests, next:
-//!'coefficient a' 'coefficient b' 'coefficient c' 'number of answer' 'answer ¹1' 'answer ¹2 ' (if roots are exist).
+//!'coefficient a' 'coefficient b' 'coefficient c' 'number of answer' 'answer 1' 'answer 2' (if roots are exist).
 //!The output accuracy is 2 significant digits after the decimal point
 
 
 
-//#define DEBUG_MODE
 
 //! the error value to compare 2 numbers
 
-const float Pogranichnik = 0.01;
+const float Pogreshnik = 0.01;
 
 
 //! the error value for comparison with 0
 
-const float Zoro =0.0001;
+//Buenos Dias
+
+const float Zoro = 0.0001;
 
 //! declaring an error for not finding a file
 
@@ -36,17 +40,17 @@ enum ERRORS
  *   \return In the function fails, the return value 1, else return value 0
  */
 
-int debajim_yohoo();
+int debajim_yOhOO();
 
 /*!
  *   \brief Checks the number#1 for equality with number#2, the 3d argument is accuracy value
  *   \param[in] number1 - the first number for comparison
  *   \param[in] number2- the second number for comparison
  *   \param[in] fallibility - accuracy value, for example 0.01 or 0.00001
- *   \return the truthfulness of equality
+ *   \return the truthfulness(VO KAK!) of equality
  */
 
-bool srav(const float number1, const float number2, const float fallibility);
+bool srav_tWo_numbErs(const float number1, const float number2, const float fallibility);
 
 
 /*!
@@ -108,7 +112,7 @@ void outputVJUX(const int KOL_otv, const float x1, const float x2);
  *   \return In the function fails, the return value 1, else return value 0
  */
 
-int TESTinpuT_file(float *a, float *b, float *c, int *KOL_otv, float *x1, float *x2, FILE *in);
+int TEST_inpuT_file(float *a, float *b, float *c, int *KOL_otv, float *x1, float *x2, FILE *in);
 
 /*!
  *   \brief output tests from debug
@@ -121,6 +125,6 @@ int TESTinpuT_file(float *a, float *b, float *c, int *KOL_otv, float *x1, float 
  *   \param[in] KOL_otv_function - number of roots of equation from function
  */
 
-void vivoddebugga(const int number, const int KOL_otv_file, const float x1, const float x2, const float x1v, const float x2v, const int KOL_otv_function);
+void vivod_debugga(const int number, const int KOL_otv_file, const float x1, const float x2, const float x1v, const float x2v, const int KOL_otv_function);
 
 
